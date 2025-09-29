7eeacb9c2d79af4488ee4e9eaeff441f4b4fe0d9
sivchheng16 <kheangsivechheng@gmail.com>
2025-09-29 20:03:46 +0700

    update

--- react-learn/vite.config.ts
+++ react-learn/vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  preview: {
+    host: "0.0.0.0", // Make it accessible from external network
+    allowedHosts: ["reactcomponent.koompi.cloud", "localhost", "0.0.0.0"], // Allow these hosts
+  },
 });
